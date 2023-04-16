7efee7f944c9520d88c0971b6b29c384cf277908
uejo <rentarou1994@gmail.com>
2023-04-16 17:42:26 +0900

    :tada: Initial Commit!!

---
+++ app/src/main/java/com/websarva/wings/android/helloandroid/ListenerLearningActivity.java
@@ -0,0 +1,14 @@
+package com.websarva.wings.android.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListenerLearningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listener_learning);
+    }
+}
